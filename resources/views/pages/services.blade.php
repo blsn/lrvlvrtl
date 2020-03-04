4c4a6cc578d22e93cc205d9f788ecbfbfdef3ba9
Moshe Balasiano <mbalasiano@leolane.com>
2020-03-04 14:57:44 +0200

    layouts

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{config('app.name', 'lrvlvrtl')}}</title>
-    </head>
-    <body>
-        <h1>Services</h1>
-        <p>This is services page</p>
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <h1>Services</h1>
+    <p>This is services page</p>
+@endsection

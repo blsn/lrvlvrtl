34d6e8e5295425b8939f49bf78c8fc50545fcdc6
Moshe Balasiano <mbalasiano@leolane.com>
2020-03-11 11:28:08 +0200

    jumbotron

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,12 +3,16 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="{{asset('js/app.js')}}" defer></script> <!--/ add for bootstrap-->
         <title>{{config('app.name', 'lrvlvrtl')}}</title>
         <link href="{{asset('css/app.css')}}" rel="stylesheet">
     </head>
     <body>
+        @include('inc.navbar')
         <div class="container">
-            @yield('content')
-        </div>        
+            <main class="py-4">
+                @yield('content')
+            </main>
+        </div>
     </body>
 </html>

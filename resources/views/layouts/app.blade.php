f2071c450e8a0dea94359d2c802906d4b4bbb245
Moshe Balasiano <mbalasiano@leolane.com>
2020-03-23 20:50:35 +0200

    authentication

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,26 +1,31 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script src="{{asset('js/app.js')}}" defer></script> <!--/ add for bootstrap-->
-        <title>{{config('app.name', 'lrvlvrtl')}}</title>
-        <link href="{{asset('css/app.css')}}" rel="stylesheet">
-    </head>
-    <body>
-        @include('inc.navbar')
-        <div class="container">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">     <!-- CSRF Token -->
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    @include('inc.navbar')
+    <div class="container">
+        <div id="app">        
             <main class="py-4">
                 @include('inc.messages')
                 @yield('content')
             </main>
         </div>
-        <script src="//cdn.ckeditor.com/4.14.0/standard/ckeditor.js"></script>
-        <script>
-            var elementExists = document.getElementById("article-ckeditor");
-            if (elementExists) {
-                CKEDITOR.replace('article-ckeditor');
-            }
-        </script>
-    </body>
+    </div>
+    <script src="//cdn.ckeditor.com/4.14.0/standard/ckeditor.js"></script>
+    <script>
+        var elementExists = document.getElementById("article-ckeditor");
+        if (elementExists) {
+            CKEDITOR.replace('article-ckeditor');
+        }
+    </script>
+</body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,7 @@ Route::get('/services', 'PagesController@services');
 Route::resource('posts', 'PostsController');
 
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');

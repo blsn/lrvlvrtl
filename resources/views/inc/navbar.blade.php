c27150005ee16dbc3e0c04cdc2bcb3008a196d3c
Moshe Balasiano <mbalasiano@leolane.com>
2020-03-24 15:34:12 +0200

    home to dashboard

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -4,7 +4,6 @@
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
 			<span class="navbar-toggler-icon"></span>
 		</button>
-
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<!-- Left Side Of Navbar -->
 			<ul class="navbar-nav mr-auto">
@@ -20,16 +19,12 @@
 				<li class="nav-item">
 					<a class="nav-link" href="/posts">Blog</a>
 				</li>    
-			</ul>
-
-			<!-- Right Side Of Navbar -->
-			<ul class="navbar-nav ml-auto">
+			</ul>			
+			<ul class="navbar-nav ml-auto"> <!-- Right Side Of Navbar -->
 				<li class="nav-item">
 					<a class="nav-link" href="/posts/create">Create post</a>
 				</li>
-
-				<!-- Authentication Links -->
-				@guest
+				@guest <!-- Authentication Links -->
 					<li class="nav-item">
 						<a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
 					</li>
@@ -43,14 +38,12 @@
 						<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
 							{{ Auth::user()->name }} <span class="caret"></span>
 						</a>
-
 						<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
 							<a class="dropdown-item" href="{{ route('logout') }}"
 							   onclick="event.preventDefault();
 											 document.getElementById('logout-form').submit();">
 								{{ __('Logout') }}
 							</a>
-
 							<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
 								@csrf
 							</form>

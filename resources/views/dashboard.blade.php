e47c623af54244677c69a83948339f1c40ab1b35
Moshe Balasiano <mbalasiano@leolane.com>
2020-03-26 13:38:06 +0200

    user authentication

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,20 +1,18 @@
 @extends('layouts.app')
-
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
-
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
-                    @endif
-
-                    You are logged in!
+                    @endif <!-- user logged in -->
+                    <p><a class="btn btn-primary" href="/posts/create">Create post</a></p> 
+                    <h3 class="card-text">Your blog posts</h3>
                 </div>
             </div>
         </div>
--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -21,9 +21,6 @@
 				</li>    
 			</ul>			
 			<ul class="navbar-nav ml-auto"> <!-- Right Side Of Navbar -->
-				<li class="nav-item">
-					<a class="nav-link" href="/posts/create">Create post</a>
-				</li>
 				@guest <!-- Authentication Links -->
 					<li class="nav-item">
 						<a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
@@ -39,6 +36,7 @@
 							{{ Auth::user()->name }} <span class="caret"></span>
 						</a>
 						<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="/dashboard">Dashboard</a>
 							<a class="dropdown-item" href="{{ route('logout') }}"
 							   onclick="event.preventDefault();
 											 document.getElementById('logout-form').submit();">
